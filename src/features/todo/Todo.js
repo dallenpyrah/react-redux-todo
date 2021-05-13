85d5b37da4be913738662084200a2ae73fc0bf91
Dallen Pyrah <dallenpyrah@gmail.com>
2021-05-13 11:04:01 -0600

    Can create and delete todos

--- src/features/todo/Todo.js
+++ src/features/todo/Todo.js
@@ -1,9 +1,37 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { create, edit, remove, toggleComplete } from './TodoSlice'
 
 const Todo = () => {
+    const [inputText, setInputText] = useState('')
+    const dispatch = useDispatch()
+    const todos = useSelector(state => state.todos)
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        // TODO: Finish this todo submit 
+        dispatch(create(inputText))
+        setInputText('')
+    }
+    
+    const handleRemove = (id) => (e) => {
+        e.preventDefault()
+        dispatch(remove(id))
+    }
+
     return (
-        <div>Todo</div>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input onChange={e => setInputText(e.target.value)} value={inputText}/> 
+                <button type="submit">Create Todo</button>
+            </form>
+            {todos.map(todo =>(
+                <div key={todo.id}>
+                    {todo.description} {todo.isComplete ? "DONE" : ""}
+                    <button onClick={handleRemove(todo.id)}>Delete</button>
+                </div>
+            ))}
+        </div>
     )
 }
 
